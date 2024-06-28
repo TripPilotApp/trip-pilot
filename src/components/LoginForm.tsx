65bfdecd0e9928135d5042dca6c97bf7399a1007
Padma <padmalathatce@gmail.com>
2024-06-27 17:57:44 -0700

    Login functionality

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ReusableInput from "./ReusableInput";
 import { LinkButton } from "./ui/LinkButton";
+import axios from "axios";
 
 const LoginForm: React.FC = () => {
   interface FormValues {
@@ -18,12 +19,20 @@ const LoginForm: React.FC = () => {
   const [errors, setErrors] = useState<FormErrors>({});
   const [touched, setTouched] = useState<Partial<FormValues>>({});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
   };
 
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleBlur = (
+    e: React.FocusEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     const { name } = e.target;
     setTouched({ ...touched, [name]: true });
 
@@ -53,16 +62,33 @@ const LoginForm: React.FC = () => {
     setErrors({ ...errors, [name]: error });
   };
 
+  const baseURL = "http://localhost:5000/api";
+  // Handle Login On click
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    const reqBody = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+
+    const response = await axios.post(`${baseURL}/login`, reqBody);
+    console.log(response.data);
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div className="inline-flex flex-col justify-center items-center form-card border-bg">
         <h1 className="font-bold text-blue text-xl lg:text-2xl font-quicksand tracking-normal md:tracking-widest leading-none md:leading-normal lg:leading-loose mr-2 -mt-4">
           Login
         </h1>
-        <form className="flex flex-col justify-center">
+        <form className="flex flex-col justify-center" onSubmit={handleLogin}>
           <div className="flex flex-col gap-4">
             <div className="flex flex-col justify-end items-start gap-y-1">
-              <label htmlFor="email" className="text-base md:text-md font-medium font-sora">
+              <label
+                htmlFor="email"
+                className="text-base md:text-md font-medium font-sora"
+              >
                 Email
               </label>
               <ReusableInput
@@ -78,7 +104,10 @@ const LoginForm: React.FC = () => {
               )}
             </div>
             <div className="flex flex-col justify-end items-start gap-y-1">
-              <label htmlFor="password" className="text-base md:text-md font-medium font-sora">
+              <label
+                htmlFor="password"
+                className="text-base md:text-md font-medium font-sora"
+              >
                 Password
               </label>
               <ReusableInput
@@ -90,20 +119,30 @@ const LoginForm: React.FC = () => {
                 onBlur={handleBlur}
               />
               {touched.password && errors.password && (
-                <div className="text-red-500 text-sm mt-1">{errors.password}</div>
+                <div className="text-red-500 text-sm mt-1">
+                  {errors.password}
+                </div>
               )}
             </div>
           </div>
           <div className="flex items-center h-5 mt-2">
-            <input id="default-checkbox" type="checkbox" className="w-4 h-4 rounded-md border-bg" />
-            <label htmlFor="default-checkbox" className="ml-2 font-medium text-black tracking-widest text-base">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              className="w-4 h-4 rounded-md border-bg"
+            />
+            <label
+              htmlFor="default-checkbox"
+              className="ml-2 font-medium text-black tracking-widest text-base"
+            >
               Remember me
             </label>
           </div>
           <div className="flex-center mt-4">
-            <LinkButton to="/" intent="blue"  className="flex items-center justify-center w-full">
+            {/* <LinkButton to="/" intent="blue"  className="flex items-center justify-center w-full">
               Login
-            </LinkButton>
+            </LinkButton> */}
+            <button>LogIn</button>
           </div>
         </form>
         <div className="flex-center text-sm font-sora font-semibold mt-4">
